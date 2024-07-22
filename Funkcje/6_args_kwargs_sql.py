fed2306982254900ce57dcc559c35269f9b29ff4
Trener19 <Trener19@aa.labs>
2024-07-22 10:39:44 +0200

    1

---
+++ Funkcje/6_args_kwargs_sql.py
@@ -0,0 +1,23 @@
+def stworz_zapytanie_sql(tabela: str, *kolumny: str, **warunki) -> str:
+
+    # SELECT
+    if kolumny:
+        kolumny_sql = ', '.join(kolumny)
+    else:
+        kolumny_sql = '*'
+
+    # WHERE
+    warunki_sql = ' AND '.join([f"{klucz}='{wartosc}'" for klucz, wartosc in warunki.items()])
+
+    # Pełne zapytanie
+    if warunki_sql:
+        zapytanie = f'SELECT {kolumny_sql} FROM {tabela} WHERE {warunki_sql};'
+    else:
+        zapytanie = f'SELECT {kolumny_sql} FROM {tabela};'
+
+    return zapytanie
+
+
+zapytanie1 = stworz_zapytanie_sql("uzytkownicy", "imie", "nazwisko", wiek=30)
+print(zapytanie1)
+
