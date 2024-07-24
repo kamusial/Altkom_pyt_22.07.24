4fcbe0923a09f7b237d087956a0c6968f4f23f3c
Trener19 <Trener19@aa.labs>
2024-07-24 14:59:27 +0200

    1

--- 1_pandas/1_otodom.py
+++ 1_pandas/1_otodom.py
@@ -6,5 +6,7 @@ df = pd.read_csv('otodom.csv')
 print(df)
 
 print(df.describe().T.to_string())
-
+print(df.iloc[1:5, 2:4])
+print(df.describe().T.loc['price', '25%'])
+print(df.iloc[:, 1:].corr())
 
