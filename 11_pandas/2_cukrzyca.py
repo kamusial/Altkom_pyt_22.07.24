2d3555a5403e0a3407039ab95dcf0df056f6aecf
Trener19 <Trener19@aa.labs>
2024-07-25 09:21:23 +0200

    1

--- 11_pandas/2_cukrzyca.py
+++ 11_pandas/2_cukrzyca.py
@@ -10,7 +10,6 @@ print(df.describe().T.to_string())
 print(df.isna().sum())
 
 # wszędzie, gdzie są zera lub brak wartości - przypiszmy średnią
-
 for col in ['glucose', 'bloodpressure', 'skinthickness', 'insulin',
        'bmi', 'diabetespedigreefunction', 'age']:
     df[col].replace(0, np.NaN, inplace=True)
@@ -29,3 +28,20 @@ model = LogisticRegression()
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('sprawdźmy, czy klasy sa zbalansowane')
+print(df.outcome.value_counts())
+
+print('zmiana danych')
+df1 = df.query('outcome == 0').sample(500)    # 500 zdrowych
+df2 = df.query('outcome == 1').sample(500)    # 500 chorych
+df3 = pd.concat([df1, df2])                   # łączenie
+
+X = df3.iloc[:, :-1]
+y = df3.outcome
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
